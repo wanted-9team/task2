889c27eb3072e2ca50fe71336b58931184f35683
sacultang <sacultang@gmail.com>
2022-09-03 19:04:51 +0900

    [ADD] Option 추가 창 Option 버튼 클릭시 Option추가 생성, Issue => 옵션창 여러개 생성 후 작성시 기존 값을 뒤덮음

--- src/components/Admin/AdminRegister/AdminRegister.jsx
+++ src/components/Admin/AdminRegister/AdminRegister.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from './AdminRegister.style'
-import InputField from './InputField'
-import { AiOutlineClose } from 'react-icons/ai'
+import InputField from './AdminInputField'
+import AdminOption from './AdminOption'
+
 const statusData = [
   { id: 1, type: 'SALE' },
   { id: 2, type: 'BEST' },
@@ -12,51 +13,113 @@ const statusData = [
   { id: 7, type: '특가' },
   { id: 8, type: '판매대기' },
 ]
-
+const initialDataType = {
+  name: '',
+  imageUrl: [],
+  quantity: 0,
+  price: 0,
+  status: [],
+  sale: 0,
+  select: [{ option: '', price: 0 }],
+  description: [],
+  origin: '',
+  shipping: { option: '', price: 0, info: '' },
+  visible: true,
+}
 const AdminRegister = () => {
+  const [productData, setProductData] = useState(initialDataType)
+  const [createOptionDiv, setCreateOptionDiv] = useState([])
+  useEffect(() => {
+    console.log(productData)
+  }, [productData])
+
+  const handleAddOption = () => {
+    let countArr = [...createOptionDiv]
+    let counter = countArr.slice(-1)[0]
+    counter += 1
+    countArr.push(counter)
+    setCreateOptionDiv(countArr)
+  }
   return (
     <S.RegisterContaiDiv>
       <S.BoxDiv>
-        <InputField label="상품명" id="productName" type="text" />
-        <InputField label="가격" id="productPrice" type="number" />
+        <InputField setProductData={setProductData} label="상품명" id="name" type="text" />
+        <InputField setProductData={setProductData} label="가격" id="price" type="number" />
         <S.HalfBox>
-          <InputField label="수량" id="productQuantity" type="number" attr="half" />
-          <InputField label="할인율" id="productSale" type="number" attr="half" />
+          <InputField
+            setProductData={setProductData}
+            label="수량"
+            id="quantity"
+            type="number"
+            attr="half"
+          />
+          <InputField
+            setProductData={setProductData}
+            label="할인율"
+            id="sale"
+            type="number"
+            attr="half"
+          />
         </S.HalfBox>
-        <InputField label="상세설명" id="description" type="text" textArea={true} />
+        <InputField
+          setProductData={setProductData}
+          label="상세설명"
+          id="description"
+          type="text"
+          textArea={true}
+        />
 
         <S.Label>
           <S.LabelText> 상품 옵션</S.LabelText>
-          <S.OptionButton>옵션 추가</S.OptionButton>
+          <S.OptionButton onClick={handleAddOption}>옵션 추가</S.OptionButton>
         </S.Label>
-
-        <S.OptionBoxDiv>
-          <S.OptionHeader>
-            <S.Label>필수</S.Label>
-            <AiOutlineClose />
-          </S.OptionHeader>
-          <S.HalfBox>
-            <InputField label="옵션명" id="optionName" type="text" attr="option" />
-            <InputField label="옵션 가격" id="optionPrice" type="number" attr="option" />
-          </S.HalfBox>
-        </S.OptionBoxDiv>
+        {createOptionDiv &&
+          createOptionDiv.map((div, idx) => (
+            <AdminOption
+              key={idx}
+              setProductData={setProductData}
+              createOptionDiv={createOptionDiv}
+            />
+          ))}
       </S.BoxDiv>
       <S.BoxDiv>
         <S.Label>
           <S.LabelText>상품 이미지</S.LabelText>
           <S.OptionButton>파일 선택</S.OptionButton>
         </S.Label>
-        <InputField label="원산지" id="origin" type="text" />
-        <InputField label="배송 방법" id="shippingOption" type="text" />
+        <InputField setProductData={setProductData} label="원산지" id="origin" type="text" />
+        <InputField
+          setProductData={setProductData}
+          label="배송 방법"
+          id="shippingOption"
+          type="text"
+        />
         <S.ShippingBoxDiv>
-          <InputField label="배송비" id="shippingPrice" type="number" />
-          <InputField label="무료" id="shippingPriceCheck" type="checkbox" attr="shippingFree" />
+          <InputField
+            setProductData={setProductData}
+            label="배송비"
+            id="shippingPrice"
+            type="number"
+          />
+          <InputField
+            setProductData={setProductData}
+            label="무료"
+            id="shippingPriceCheck"
+            type="checkbox"
+            attr="shippingFree"
+          />
         </S.ShippingBoxDiv>
-        <InputField label="배송 안내" id="shippingInfo" type="text" />
+        <InputField
+          setProductData={setProductData}
+          label="배송 안내"
+          id="shippingInfo"
+          type="text"
+        />
         <S.LabelText>상태</S.LabelText>
         <S.StatusBox>
           {statusData.map(sts => (
             <InputField
+              setProductData={setProductData}
               label={sts.type}
               id={sts.type}
               key={sts.id}
