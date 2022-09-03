c383d3a667b0f82de3fbcaa27c0b6a65ae61fe05
sacultang <sacultang@gmail.com>
2022-09-03 16:41:58 +0900

    [ADD] AdminRegister Css Style 기본구조 작업 및 컴포넌트화 작업 직전 코드

--- src/components/Admin/AdminRegister/AdminRegister.jsx
+++ src/components/Admin/AdminRegister/AdminRegister.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
 import * as S from './AdminRegister.style'
 
+const statusData = [
+  { id: 1, type: 'SALE' },
+  { id: 2, type: 'BEST' },
+  { id: 3, type: 'MD' },
+  { id: 4, type: 'SOLDOUT' },
+  { id: 5, type: 'NEW' },
+  { id: 6, type: '무료배송' },
+  { id: 7, type: '특가' },
+  { id: 8, type: '판매대기' },
+]
+const InputField = ({ label, id, type, attr, textArea }) => {
+  return (
+    <S.Label htmlFor={id} attr={attr}>
+      <S.LabelText>{label}</S.LabelText>
+      {textArea ? <S.TextArea type={type} id={id} /> : <S.TextInput id={id} type={type} />}
+    </S.Label>
+  )
+}
 const AdminRegister = () => {
-  return <div>관리자 상품등록</div>
+  return (
+    <S.RegisterContaiDiv>
+      <S.BoxDiv>
+        <InputField label="상품명" id="productName" type="text" />
+        <InputField label="가격" id="productPrice" type="number" />
+        <S.HalfBox>
+          <InputField label="수량" id="productQuantity" type="number" attr="half" />
+          <InputField label="할인율" id="productSale" type="number" attr="marginLeft" />
+        </S.HalfBox>
+        <InputField label="상세설명" id="description" type="text" textArea={true} />
+
+        <S.Label htmlFor="">상품 옵션</S.Label>
+        <S.OptionButton>옵션 추가</S.OptionButton>
+        <S.OptionBoxDiv>
+          <S.OptionHeader>
+            <S.Label>필수</S.Label>
+            <button>Close</button>
+          </S.OptionHeader>
+          <S.HalfBox>
+            <InputField label="옵션명" id="optionName" type="text" attr="half" />
+            <InputField label="옵션 가격" id="optionPrice" type="number" attr="half" />
+          </S.HalfBox>
+        </S.OptionBoxDiv>
+      </S.BoxDiv>
+      <S.BoxDiv>
+        <S.Label htmlFor="">상품 이미지</S.Label>
+        <S.OptionButton>파일 선택</S.OptionButton>
+        <InputField label="원산지" id="origin" type="text" />
+        <InputField label="배송 방법" id="shippingOption" type="text" />
+        <InputField label="배송비" id="shippingPrice" type="number" />
+
+        <S.StatusBox>
+          {statusData.map(sts => (
+            <S.Label>
+              <S.CheckInput type="checkbox" />
+              {sts.type}
+            </S.Label>
+          ))}
+        </S.StatusBox>
+      </S.BoxDiv>
+    </S.RegisterContaiDiv>
+  )
 }
 
 export default AdminRegister
--- src/components/Admin/AdminRegister/AdminRegister.style.js
+++ src/components/Admin/AdminRegister/AdminRegister.style.js
@@ -1,1 +1,81 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const RegisterContaiDiv = styled.div`
+  display: flex;
+  width: 1200px;
+  background-color: #eee;
+  margin: 0 auto;
+  padding: 73px 100px;
+  justify-content: space-between;
+`
+
+export const BoxDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 458px;
+`
+export const HalfBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+  ${({ attr }) => {
+    switch (attr) {
+      case 'half':
+        return css`
+          width: 209px;
+        `
+      case 'marginLeft':
+        return css`
+          margin-left: 17px;
+          flex: 1;
+        `
+      default:
+        return css`
+          width: 100%;
+          flex: 1;
+        `
+    }
+  }}
+`
+export const LabelText = styled.h3`
+  padding: 5px 0;
+`
+export const TextInput = styled.input`
+  height: 42px;
+  border-radius: 4px;
+  border: 1px solid #a1a1a1;
+`
+
+export const TextArea = styled.textarea`
+  height: 227px;
+  resize: none;
+  border-radius: 4px;
+  border: 1px solid #a1a1a1;
+`
+export const StatusBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+export const CheckInput = styled.input``
+
+export const OptionButton = styled.button`
+  height: 42px;
+  background-color: #a1a1a1;
+  border-radius: 4px;
+`
+export const OptionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const OptionBoxDiv = styled.div`
+  width: 100%;
+  background-color: #fff;
+  padding: 18px 15px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+`
