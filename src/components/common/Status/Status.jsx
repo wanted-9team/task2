25c46b3a3034ec3d3abb4f5b68330870d59901fd
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-09-03 12:15:37 +0900

    [ADD]Status박스 추가
    스토어페이지와 어드민에서 공용으로 사용되는용 상태박스를 구현

---
+++ src/components/common/Status/Status.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as S from './Status.style'
+
+const Status = ({ status }) => {
+  return <S.StatusContainer>{status}</S.StatusContainer>
+}
+
+export default Status
