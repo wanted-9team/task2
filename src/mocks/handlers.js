a669677445dace6b306ce3d374983b475e6cfd31
sacultang <sacultang@gmail.com>
2022-09-04 21:23:34 +0900

    [ADD] Err 케이스 추가 파라미터 및 데이터 없을시 400에러 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,40 +1,61 @@
 import { rest } from 'msw'
-import data from './data'
+import productData from './data'
 import orderData from './orderData'
 
 export const handlers = [
   rest.get(`/productlist`, (req, res, ctx) => {
     const page = req.url.searchParams.get('page')
     const result = []
-    for (let i = 0; i < data.length; i += 10) {
-      result.push(data.slice(i, i + 10))
-    }
-    const returnPageData = result[Number(page) - 1]
+    const visibleData = productData.filter(item => item.visible)
 
+    for (let i = 0; i < visibleData.length; i += 10) {
+      result.push(visibleData.slice(i, i + 10))
+    }
+
+    const returnPageData = result[Number(page) - 1]
+    if (!page) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: 'required parameter page is not present',
+        }),
+      )
+    }
     return res(
       ctx.status(200),
-      ctx.json({ list: returnPageData, totalPage: result.length, totalResults: data.length }),
+      ctx.json({
+        list: returnPageData,
+        totalPage: result.length,
+        totalResults: visibleData.length,
+      }),
     )
   }),
 
   rest.post('/createproduct', (req, res, ctx) => {
     const body = req.body
-
-    if (!body) {
-      return res(ctx.status(400), ctx.json({ message: 'No-Data' }))
+    const { data } = body
+    if (!data.name || !data.price || !data.quantity || !data.shipping.price) {
+      return res(ctx.status(400), ctx.json({ message: 'required data is not present' }))
     }
 
-    data.push({ ...body, id: data.length + 1 })
-    return res(ctx.status(200), ctx.json({ message: '등록되었습니다.' }))
+    productData.push({ ...body, id: data.length + 1 })
+    return res(ctx.status(200), ctx.json({ message: '등록되었습니다.', product: data }))
   }),
 
   rest.delete('/deleteproduct', (req, res, ctx) => {
     const id = req.url.searchParams.get('id')
-    data.splice(
-      data.findIndex(item => item.id === Number(id)),
+    productData.splice(
+      productData.findIndex(item => item.id === Number(id)),
       1,
     )
-
+    if (!id) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: 'required parameter id is not present',
+        }),
+      )
+    }
     return res(ctx.status(204))
   }),
   rest.get(`/orderlist`, (req, res, ctx) => {
