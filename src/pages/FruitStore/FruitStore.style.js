5f16cb5bcb3ee299fa4ede2f2db501b3ae03943f
hlezg08 <hyelin132@gmail.com>
2022-09-04 23:27:37 +0900

    [ADD] 캐러셀 컴포넌트 추가 및 텍스트를 캐러셀로 이동
    상품목록 하단 텍스트가 눈에 띄지 않아 최상단 캐러셀로 이동
    캐러셀로 여러 홍보 문구 표시

--- src/pages/FruitStore/FruitStore.style.js
+++ src/pages/FruitStore/FruitStore.style.js
@@ -16,30 +16,3 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 100px;
 `
-export const TextTable = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 50px 0;
-`
-export const TextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 15px 0;
-`
-export const TextStrong = styled.h5`
-  font-size: 1.75rem;
-  font-weight: 700;
-  margin: 10px 0;
-`
-export const Text = styled.p`
-  font-size: 1rem;
-  color: #404040;
-  line-height: 27px;
-`
-export const LogoImg = styled.img`
-  width: 35px;
-  height: 35px;
-  margin: 10px 0;
-`
