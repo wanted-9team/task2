63cd332f8444b24b7eac4cec4c1bb28089ac414a
estherjj <71773680+estherjj@users.noreply.github.com>
2022-09-05 00:04:59 +0900

    [FIX] header 컴포넌트 스타일 수정 (#33)
    * [FIX] Nav 링크 스타일 오류 수정
    
    * [ADD] header 컴포넌트 스크롤 효과 추가
    
    - 스크롤을 감지하여 header 컴포넌트 노출/비노출되는 기능 추가

--- src/components/common/Headers/Headers.style.js
+++ src/components/common/Headers/Headers.style.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom'
 export const Header = styled.header`
   position: fixed;
   top: 0;
+  width: 100%;
+  height: 90px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  height: 90px;
-  padding: 0 40px;
   border-bottom: 1px solid #eee;
   background-color: #fff;
   z-index: 100;
@@ -20,9 +19,11 @@ export const NavWrap = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  max-width: 1200px;
 `
 
 export const LogoLink = styled(Link)`
+  margin-left: 20px;
   display: flex;
   align-items: center;
   width: 179px;
@@ -41,13 +42,17 @@ export const Nav = styled.nav`
 
 export const StoreLink = styled(Link)`
   margin-right: 14px;
-  padding: 20px 20px;
+  padding: 20px;
   display: block;
   font-size: 18px;
   color: #111;
   font-weight: 700;
   :hover {
     color: #5f9a3f;
+  }
+  @media screen and (max-width: 414px) {
+    padding: 10px;
+    font-size: 16px;
   }
 `
 
@@ -58,5 +63,9 @@ export const AdminLink = styled(Link)`
   color: #111;
   :hover {
     color: #5f9a3f;
+  }
+  @media screen and (max-width: 414px) {
+    padding: 10px;
+    font-size: 16px;
   }
 `
