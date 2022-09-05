92ba16188688b3db1748af7c35a6f0315d495519
Chaejung Kim <whenucan35@gmail.com>
2022-09-05 10:36:56 +0900

    [ADD] 쇼핑 계속하기 navigate (#46)
    * [FIX] import error fix
    
    merge 중 누락된 Import 문을 추가합니다.
    
    * [STY] icon inline size 추가
    
    * [ADD] 쇼핑 계속하기 navigate 추가
    
    * [CHO] shopList 스타일 수정
    
    * [ADD] 가격 포매팅 추가

--- src/pages/ShopList/ShopList.jsx
+++ src/pages/ShopList/ShopList.jsx
@@ -1,11 +1,18 @@
-import useProductApi from '../../utils/useProductApi'
 import { BiCheckCircle } from 'react-icons/bi'
+import * as S from './ShopList.style'
+import { useNavigate } from 'react-router-dom'
+import { getFormattedPrice } from '../../utils/getPrice'
 
 const ShopList = () => {
+  const navigate = useNavigate()
+  const goShoppingPage = () => {
+    navigate('/fruit_store')
+  }
+
   return (
     <>
       <S.MainContainer>
-        <BiCheckCircle />
+        <BiCheckCircle size="120px" />
         <S.MessageTitle>주문이 완료되었습니다.</S.MessageTitle>
         <S.OrderDetailContainer>
           <S.OrderDetailTitle>
@@ -27,7 +34,7 @@ const ShopList = () => {
                     <S.OptionDetail>쇼핑백 - 1개</S.OptionDetail>
                   </S.OrderOption>
                 </S.OrderProductDescription>
-                <S.OrderProductPrice>36000원</S.OrderProductPrice>
+                <S.OrderProductPrice>{getFormattedPrice(36000)}원</S.OrderProductPrice>
               </S.OrderProductBodyContainer>
             </S.Product>
             <S.OrderDetailButton>더보기</S.OrderDetailButton>
@@ -40,15 +47,15 @@ const ShopList = () => {
           <S.PaymentInfo>
             <S.DetailLine>
               <S.Type>총 결제금액</S.Type>
-              <S.Value $option="totalPrice">18700원</S.Value>
+              <S.Value $option="totalPrice">{getFormattedPrice(18700)}원</S.Value>
             </S.DetailLine>
             <S.DetailLine>
               <S.Type>총 상품가격</S.Type>
-              <S.Value>15200원</S.Value>
+              <S.Value>{getFormattedPrice(15200)}원</S.Value>
             </S.DetailLine>
             <S.DetailLine>
               <S.Type>배송비</S.Type>
-              <S.Value>3500원</S.Value>
+              <S.Value>{getFormattedPrice(3500)}원</S.Value>
             </S.DetailLine>
           </S.PaymentInfo>
         </S.OrderDetailContainer>
@@ -71,7 +78,7 @@ const ShopList = () => {
             </S.DetailLine>
           </S.PaymentInfo>
         </S.OrderDetailContainer>
-        <S.BacktoShoppingButton>쇼핑 계속하기</S.BacktoShoppingButton>
+        <S.BacktoShoppingButton onClick={goShoppingPage}>쇼핑 계속하기</S.BacktoShoppingButton>
       </S.MainContainer>
     </>
   )
